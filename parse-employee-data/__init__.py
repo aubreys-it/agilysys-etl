c49f0f991ee6cc594a705608ebc89f3546e12467
Aubrey's IT <95295945+aubreys-it@users.noreply.github.com>
2025-02-05 16:33:54 +0000

    Tidy Up

--- parse-employee-data/__init__.py
+++ parse-employee-data/__init__.py
@@ -35,12 +35,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
         for line in txt_data:
             #Get Employee Header Information
-            emp_id = line[line.find(',')+1:line.find(' ', line.find(',')+1)]
-            emp_line = line[:line.find('{')] + line[line.find('}')+1:]
+            emp_id = line[line.find(',')+1:line.find(',', line.find(',')+1)]
+            emp_line = line[:line.find('{')] + line[line.find('}')+2:]
             emp_csv_lines.append(emp_line)
 
             #Get ROP Information
-            rop_line = line[line.find('{')+1:line.find('}')]
+            rop_line = line[line.find('{')+1:line.find('}')].replace('$','')
             rop_values = rop_line.split(',')
             for i in range(int(len(rop_values)/4)):
                 rop_csv_lines.append(f'{emp_id},{rop_values[i*4]},{rop_values[i*4+1]},{rop_values[i*4+2]},{rop_values[i*4+3]}')
