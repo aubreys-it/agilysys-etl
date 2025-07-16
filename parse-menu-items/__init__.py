5b94d10092844f9a1ae989d493245115f2436dd0
Aubrey's IT <95295945+aubreys-it@users.noreply.github.com>
2025-07-16 14:21:20 +0000

    Fix Nulls 2

--- parse-menu-items/__init__.py
+++ parse-menu-items/__init__.py
@@ -46,7 +46,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         for line in txt_data:
             #Get Menu Item Header Information
             item_id = line[line.find(',')+1:line.find(',', line.find(',')+1)]
-            mi_line = loc_id + ',' + re.sub(",{.*?}", "", line).replace('$','').replace(",,",",NULL," )
+            mi_line = loc_id + ',' + re.sub(",{.*?}", "", line).replace('$','').replace('""','')
             mi_csv_lines.append(mi_line)
 
             #Get Price Level Information
